201ef3b70a2dd5dc81f4a1c73cddf002d4ee3036
tuurss5lavx2 <10804966+tuurss5lavx2@users.noreply.github.com>
2025-09-21 08:20:37 -0300

    update

--- projeto-node/config/database.js
+++ projeto-node/config/database.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 connection.connect((err) => {
