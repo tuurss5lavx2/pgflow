881efaa2cd0ca1c3bb75115314feaa29a4529421
tuurss5lavx2 <10804966+tuurss5lavx2@users.noreply.github.com>
2025-09-21 10:35:34 -0300

    update

--- projeto-node/config/database.js
+++ projeto-node/config/database.js
@@ -2,19 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao MySQL:', err.message);
-    return;
-  }
-  console.log('Conectado ao MySQL com sucesso!');
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 module.exports = connection;
