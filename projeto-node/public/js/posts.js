266272aebca1e9b3815a698bbb02b1da1dda51dc
tuurss5lavx2 <10804966+tuurss5lavx2@users.noreply.github.com>
2025-11-18 17:48:58 -0300

    up

--- projeto-node/public/js/posts.js
+++ projeto-node/public/js/posts.js
@@ -156,6 +156,7 @@ function displayPosts(posts) {
   addPostEventListeners();
   updatePostsCount(posts);
 }
+
 // Função para alternar like/unlike
 // ✅ CORREÇÃO FINAL: Função toggleLike super otimizada
 async function toggleLike(postId, button) {
