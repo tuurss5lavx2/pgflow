c211a5a659c65b54ead3c9a7961ea27dd10e83c8
tuurss5lavx2 <10804966+tuurss5lavx2@users.noreply.github.com>
2025-11-19 00:08:44 -0300

    up

--- projeto-node/server.js
+++ projeto-node/server.js
@@ -25,6 +25,7 @@ app.use(cors({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/sounds', express.static(path.join(__dirname, 'public', 'sounds'))); // ⬅️ ADICIONE ESTA LINHA
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ✅ ROTAS DA API (com logs apenas em desenvolvimento)
